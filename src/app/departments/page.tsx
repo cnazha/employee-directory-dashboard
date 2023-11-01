abe1c97e2181a2d803a702a43945e21b49d12a87
cnazha <christian@nazha.dev>
2023-11-01 16:06:32 +0200

    init - mui next template

---
+++ src/app/departments/page.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const DepartmentsPage = () => {
+  return (
+    <Container>
+      <Box>
+        <Typography variant="body1" gutterBottom>
+            Departments Page
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
+
+export default DepartmentsPage;
---
+++ src/app/employees/page.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const EmployeesPage = () => {
+  return (
+    <Container>
+      <Box>
+        <Typography variant="body1" gutterBottom>
+          Employees Page
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
+
+export default EmployeesPage;
