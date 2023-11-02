1dd9e1f47893b52f79a409b05983d88a1545a27d
cnazha <christian@nazha.dev>
2023-11-02 09:46:31 +0200

    config(env): add env

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,3 +6,6 @@ export const employeeMinAge = 18;
 // use date-fsn to get min and max dates
 export const employeeMaxBirthDate = subYears(new Date(), employeeMinAge);
 export const employeeMinBirthDate = subYears(new Date(), employeeMaxAge);
+
+
+export const GRAPHQL_URL = "http://127.0.0.1:3000/graphql";
